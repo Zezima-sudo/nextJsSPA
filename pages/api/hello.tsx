d6a5dfa2ca3407098e6a00d0dc2af034aec9eed3
Eric Rice <ericmichaelrice@protonmail.com>
2020-11-14 13:19:25 -0500

    added types to app and req,res

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,4 +1,6 @@
-export default (req, res) => {
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
     res.status(200).json({
         text: 'Hello'
     })
