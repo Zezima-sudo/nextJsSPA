95c3b157afa0ba656266f2ae981cb8500760d852
Eric Rice <ericmichaelrice@protonmail.com>
2021-01-07 14:10:16 -0500

    Lambda Labs

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,13 +33,13 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}></section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Projects</h2>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
 
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href='https://defi-dapp-farm.vercel.app/'>
-                <a>Solidity Yield Farming dApp</a>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
               </Link>
               <br />
               <small className={utilStyles.lightText}>
--- posts/pre-rendering.md
+++ posts/pre-rendering.md
@@ -1,9 +1,10 @@
 ---
 title: 'Solidity Yield Farming dApp'
-date: '2020-01-01'
+date: '2020-11-01'
 ---
 
-I built a simple yield farming dApp that utilizes React/Typescript for the front end and Truffle/Solidity for the deployment/compiling of smart contracts. 
+
+I built a simple <a href="https://defi-dapp-farm.vercel.app/">yield farming dApp</a> that utilizes **React/Typescript** for the front end and Truffle/Solidity for the deployment/compiling of smart contracts. 
 
 To build the app I created two cryptocurrencies: One is **mDai**, a simple stable coin pegged to a dollar, and the other is **Dapp**, a token used to reward our dApp's liquidity farmers. 
 
