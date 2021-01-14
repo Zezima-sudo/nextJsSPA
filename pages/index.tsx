34a3ef6a5486426293fdacd392247b76b298a963
Eric Rice <ericmichaelrice@protonmail.com>
2021-01-14 12:59:26 -0500

    newest

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}></section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog (I built this with next.js 😎<h6><a href="https://github.com/zezimaSudo/nextJsSPA">source code</a></h6>)</h2>
+        <h2 className={utilStyles.headingLg}>Blog (I built this with next.js 😎<span><a href="https://github.com/zezimaSudo/nextJsSPA">source code</a></span>)</h2>
         <ul className={utilStyles.list}>
 
           {allPostsData.map(({ id, date, title }) => (
